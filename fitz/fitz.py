8c93ff4f08e21a6eb4e7186857b28bafa7ac4763
Jorj McKie <jorj.x.mckie@outlook.de>
2021-04-10 10:42:09 -0400

    bump to v1.18.12

--- fitz/fitz.py
+++ fitz/fitz.py
@@ -102,7 +102,7 @@ except ImportError:
 
 
 VersionFitz = "1.18.0"
-VersionBind = "1.18.11"
+VersionBind = "1.18.12"
 VersionDate = "2021-04-10 04:00:00"
 version = (VersionBind, VersionFitz, "20210410040000")
 
